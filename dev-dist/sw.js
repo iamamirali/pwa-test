f825235b58e97fdbe09d4ce4ff896a3ed1c1ab34
AmirAli Shabani <iamamirali.sh@gmail.com>
2025-07-19 18:03:22 +0330

    feat: add handler logic for barcode scan

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5tf02qebs4g"
+    "revision": "0.m3dd8h5r69"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,68 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [value, setValue] = useState('');
+  const [confirmButton, setConfirmButton] = useState(false);
+  const [startButton, setStartButton] = useState(false);
+
+  useEffect(() => {
+    const handlePaste = (e: ClipboardEvent) => {
+      const pastedData = e.clipboardData?.getData('text');
+
+      if (pastedData) {
+        // setValue(pastedData.trim());
+        setConfirmButton(true);
+      }
+    };
+
+    const handleAppFocus = () => {
+      setStartButton(true);
+    };
+
+    window.addEventListener('paste', handlePaste);
+    window.addEventListener('focus', handleAppFocus);
+
+    return () => {
+      window.removeEventListener('paste', handlePaste);
+      window.removeEventListener('focus', handleAppFocus);
+    };
+  }, []);
+
   return (
     <div>
       <h1>hello world</h1>
+      <p>{`value: ${value}`}</p>
+      {confirmButton && !startButton && (
+        <button
+          onClick={() => {
+            setValue('');
+          }}
+          style={{ width: 100, height: 100 }}
+        >
+          confirm
+        </button>
+      )}
+      {startButton && (
+        <button
+          onClick={() => {
+            setStartButton(false);
+            setValue('');
+          }}
+          style={{ width: 100, height: 100 }}
+        >
+          start
+        </button>
+      )}
+      <input
+        inputMode="none"
+        value={''}
+        onChange={(e) => setValue(e.target.value)}
+        // style={{
+        //   position: 'absolute',
+        //   opacity: 0,
+        //   pointerEvents: 'none',
+        // }}
+      />
     </div>
   );
 }
